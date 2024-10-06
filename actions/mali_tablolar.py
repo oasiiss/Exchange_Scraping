6c86e4f6ef2d0c499374142fff6fccb7da7269c4
osman efe <oasiiss34@gmail.com>
2024-10-06 22:01:23 +0300

    Created AllCompany and CompanyDeatil in malitablolar.com

---
+++ actions/mali_tablolar.py
@@ -0,0 +1,96 @@
+from bs4 import BeautifulSoup
+import http.client
+
+def AllCompany(cookie, token):
+    try:
+        conn = http.client.HTTPSConnection("malitablolar.com")
+        
+        headers = {
+            'Cookie': f'.AspNetCore.Cookies={cookie}; Malitablolar={token}',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Host': 'malitablolar.com',
+            'Connection': 'keep-alive',
+            'Accept': '*/*'
+        }
+        
+        conn.request("GET", "/AraciKurumTahmin/GetAraciKurumTahminOrtamalari?kur=TL", headers=headers)
+        
+        res = conn.getresponse()
+        data = res.read()
+        
+        soup = BeautifulSoup(data, 'html.parser')
+
+        example_table = soup.find('table', id='example')
+        
+        if not example_table:
+            return [False]
+        
+        tbody = example_table.find("tbody")
+
+        if not tbody:
+            return [False]
+        
+        company_names = []
+        for row in tbody.find_all('tr'):
+            try:
+                span_tag = row.find_all('span')[-1]
+                if span_tag:
+                    company_name = span_tag.text.strip()
+                    if len(company_name) > 0:
+                        company_names.append(company_name)
+            except:
+                continue
+
+        if len(company_names) > 0:
+            return [True, company_names]
+        else:
+            return [False]
+    except:
+        return [False]
+    
+def CompanyDetail(cmp, kur, cookie, token):
+    conn = http.client.HTTPSConnection("malitablolar.com")
+
+    endpoint = f"/AraciKurumTahmin/GetAraciKurumTahminDetayBySirket?sirket={cmp}&kur={kur}"
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'Cookie': f'.AspNetCore.Cookies={cookie}; Malitablolar={token}'
+    }
+
+    conn.request("GET", endpoint, headers=headers)
+
+    response = conn.getresponse()
+    data = response.read()
+
+    conn.close()
+    
+    soup = BeautifulSoup(data, 'html.parser')
+
+    table = soup.find('table', id="example2")
+    if table:
+        tbody = table.find('tbody')
+
+        results = []
+
+        for row in tbody.find_all('tr'):
+            cols = row.find_all('td')
+            if len(cols) > 0:
+                kurum_adi = cols[0].text.strip()
+                hedef_fiyat = cols[6].text.strip()
+                tarih = cols[7].text.strip()
+
+                result = {
+                    "kurum_adi": kurum_adi,
+                    "hedef_fiyat": hedef_fiyat,
+                    "tarih": tarih
+                }
+
+                results.append(result)
+
+        if len(results) > 0:
+            return [True, results]
+        else:
+            return [False]
+    else:
+        return [False]
